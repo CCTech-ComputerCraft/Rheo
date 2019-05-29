283669f69b03693dc4b50860bb17b5c3853026fe
CCTech-ComputerCraft <46626066+CCTech-ComputerCraft@users.noreply.github.com>
2019-05-29 23:56:57 +0200

    Add files via upload

--- test.rs
+++ test.rs
@@ -1,1 +1,8 @@
-print()
+var String dict: readFile("test.dict")
+var List dict: parseDict(dict)
+
+print(conc("Clicked x: ", makestring(dict[2])))
+print(conc("Clickec y: ", makestring(dict[3])))
+print(conc("With: ", dict[1]))
+
+wait(3)
