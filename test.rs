e9778ae2ff5f846cb36505186070aaf833faecd3
CCTech-ComputerCraft <46626066+CCTech-ComputerCraft@users.noreply.github.com>
2019-05-30 00:00:25 +0200

    Delete test.rs

--- test.rs
+++
@@ -1,8 +0,0 @@
-var String dict: readFile("test.dict")
-var List dict: parseDict(dict)
-
-print(conc("Clicked x: ", makestring(dict[2])))
-print(conc("Clickec y: ", makestring(dict[3])))
-print(conc("With: ", dict[1]))
-
-wait(3)
